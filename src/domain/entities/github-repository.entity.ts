1e74635c280412b47f669b1b30bb60b3422dddbc
mncolla <maxincolla@gmail.com>
2024-11-03 04:08:21 -0300

    feat: application logic for github repository entity

---
+++ src/domain/entities/github-repository.entity.ts
@@ -0,0 +1,10 @@
+export interface Repository {
+    id: number;
+    name: string;
+    avatar: string;
+    description?: string;
+    tags?: string[];
+    language?: string;
+    stars: number;
+    activity: Date; 
+}
---
+++ src/infraestructure/mappers/github-repository.mapper.ts
@@ -0,0 +1,18 @@
+import { Repository } from "../../domain/entities/github-repository.entity";
+import { GithubRepository } from "../interfaces/github-repository.interface";
+
+export class GithubRepositoryMapper {
+
+    static githubApiToEntity(data: GithubRepository): Repository {
+        return {
+            id: data.id,
+            name: data.full_name,
+            avatar: data.owner.avatar_url,
+            description: data.description,
+            tags: data.topics,
+            language: data.language,
+            stars: data.stargazers_count,
+            activity: data.updated_at,
+        }
+    }
+}
