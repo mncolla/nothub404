4cec2f9adf612330c17d015fafcb85487b11846e
mncolla <maxincolla@gmail.com>
2024-11-04 04:41:12 -0300

    feat: add a unit format to starscount component

--- src/presentation/components/ui/StarsCount.tsx
+++ src/presentation/components/ui/StarsCount.tsx
@@ -1,12 +1,16 @@
 import { View, Text, StyleSheet } from 'react-native'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { COLORS } from '../../../config/theme/colors';
+import { formatUnitNumber } from '../../../config/helpers/formatUnitNumbers';
 
 const StarsCount = ({count}:{count: number}) => {
+
+  const formattedNumbers = formatUnitNumber(count)
+
   return (
     <View style={styles.container}>
         <AntDesign name="staro" size={14} color={COLORS.muted} />
-        <Text style={{color: COLORS.muted}}>{count}</Text>
+        <Text style={{color: COLORS.muted}}>{formattedNumbers}</Text>
     </View>
   )
 }
